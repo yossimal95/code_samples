10f2ce1f5cae64aee950c7553730e2a15fe1cb25
yossimal95 <70268960+yossimal95@users.noreply.github.com>
2025-09-14 10:22:56 +0300

    Add files via upload

---
+++ C#/.netFramework/load dll on runtime and run methods/MyClassLibrary/ClassLibrary1/TaskLogDate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class TaskLogDate : ITask
+    {
+        public string taskName
+        {
+            get
+            {
+                return "LogTheDate";
+            }
+        }
+
+        public string taskDescription
+        {
+            get
+            {
+                return "Print the current date in to Log.. bla bla..";
+            }
+        }
+
+        public void RunTask()
+        {
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+        }
+    }
+}
