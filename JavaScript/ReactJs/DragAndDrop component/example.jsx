67ab952be915f354541154a4ece28911ec1f1469
yossimal95 <70268960+yossimal95@users.noreply.github.com>
2025-09-18 11:17:49 +0300

    Update example.jsx

--- JavaScript/ReactJs/DragAndDrop component/example.jsx
+++ JavaScript/ReactJs/DragAndDrop component/example.jsx
@@ -1,12 +1,10 @@
 import DragAndDrop from "somewhere";
 
 const App = () => {
-  
-  const draggableRef = useRef(null);
-  
-  return <DragAndDrop draggbleElement={draggableRef}>
-          <div ref={draggableRef}>
-            {/* child elements.. */}
-          </div>
+  const draggbleElement = useRef(null);
+  return (
+    <DragAndDrop draggbleElement={draggbleElement}>
+      <div ref={draggbleElement} style={{ width: "100px", height: "100px", backgroundColor: "red" }}></div>
     </DragAndDrop>
+  );
 }
